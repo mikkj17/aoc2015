5b94bc94c318212be74a869f7a8cf391e7419537
Mikkel <mikkel@housekeepr.dk>
2022-09-04 11:07:11 +0200

    initial commit

---
+++ download_input.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:dotenv/dotenv.dart';
+import 'package:http/http.dart' as http;
+
+void downloadInput(String day) {
+  var url = Uri.https('adventofcode.com', '2015/day/${int.parse(day)}/input');
+  var env = DotEnv()..load();
+  var file = File('$day/input.txt');
+  file.createSync(recursive: true);
+  http.get(url, headers: {
+    'Cookie': 'session=${env['SESSION']}',
+  }).then((value) => file.writeAsString(value.body));
+}
+
+void main(List<String> args) {
+  downloadInput(args[0]);
+}
